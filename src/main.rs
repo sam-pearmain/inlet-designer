195e3560e71b934f1912ee66c376d79cc91c5c79
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-03-20 18:17:36 +0000

    cli stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::env;
+use std::io;
+use std::io::Write;
+use std::process::exit;
+
+mod taylormaccoll;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("usage: {} [inlet type]", args[0]);
+        std::process::exit(1);
+    }
+
+    let inlet_type = &args[1];
+
+    match inlet_type.as_str() {
+        "b" | "busemann" => {
+            // ask use to input inlet mach number
+            print!("enter the design exit mach number: ");
+            io::stdout().flush().unwrap();
+            
+            let mut input = String::new();
+            io::stdin().read_line(&mut input)
+                .expect("failed to read input mach");
+
+            let exit_mach: f64 = match input.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    eprintln!("invalid exit mach number");
+                    exit(1);
+                }
+            };
+            println!("{}", exit_mach)
+        }
+        "icfa" => {
+            todo!()
+        }
+        "tb" | "truncated busemann" => {
+            todo!()
+        }
+        _ => {
+            eprintln!("unknown inlet type '{}'", inlet_type);
+            exit(1);
+        }
+    }
 }
